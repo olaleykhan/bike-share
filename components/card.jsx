ddad7521b361b0ccd2999bae1a9ade1e6fed1403
olaleykhan <realjoy9@gmail.com>
2022-02-24 15:42:00 +0100

    added intro section

--- components/card.jsx
+++ components/card.jsx
@@ -1,7 +1,68 @@
 import React from 'react';
+import styled from 'styled-components';
+import Link from 'next/link';
+import Image from 'next/image';
 
-const card = () => {
-	return <div> vard here </div>;
+// import cls from 'classnames';
+
+const Card = ({ id, name, imgUrl }) => {
+	return (
+		<Link href={`${id}`} passHref>
+			<MyLink>
+				<Wrap>
+					{' '}
+					<div className={`cardHeaderWrapper`}>
+						<h2 className={`cardHeader`}>{name}</h2>
+					</div>
+					<div className={`cardImageWrapper`}>
+						<Image alt={name} className={`cardImage`} src={imgUrl} width={260} height={160} />
+					</div>{' '}
+				</Wrap>
+			</MyLink>
+		</Link>
+	);
 };
 
-export default card;
+const MyLink = styled.a`
+	margin: auto;
+	--tw-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04);
+	box-shadow: 0 0 transparent, 0 0 #0000, 0 0 transparent, 0 0 #0000, 0 0 transparent;
+
+	/* border-color: var(--text-white-100); */
+	border-radius: 0.75rem /* 12px */;
+`;
+const Wrap = styled.div`
+	border-radius: 0.75rem /* 12px */;
+	padding-top: 0.25rem /* 4px */;
+	padding-bottom: 0.25rem /* 4px */;
+	padding-bottom: 1.25rem /* 20px */;
+	padding-left: 1rem /* 16px */;
+	padding-right: 1rem /* 16px */;
+	height: 100%;
+
+	backdrop-filter: blur(10px);
+	.cardHeaderWrapper {
+		margin-top: 0.75rem /* 12px */;
+		margin-bottom: 0.75rem /* 12px */;
+	}
+
+	.cardHeader {
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		font-size: 1.25rem /* 20px */;
+		line-height: 1.75rem /* 28px */;
+		font-weight: 800;
+		overflow: hidden;
+		width: 16rem /* 256px */;
+	}
+
+	.cardImageWrapper {
+		color: var(--text-white-100);
+	}
+
+	.cardImage {
+		border-radius: 0.75rem /* 12px */;
+	}
+`;
+
+export default Card;
--- layout/home/Bikers.jsx
+++ layout/home/Bikers.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import bikersData from '../../data/bikers-data.json';
+import Card from '../../components/Card';
+import styled from 'styled-components';
 
 const Bikers = () => {
-	return <div>Bikers</div>;
+	return (
+		<Wrap>
+			{bikersData.map((biker) => (
+				<Card key={biker.id} {...biker} />
+			))}
+		</Wrap>
+	);
 };
 
+const Wrap = styled.div`
+	display: grid;
+	grid-template-columns: repeat(1, minmax(0, 1fr));
+	column-gap: 1.5rem;
+	row-gap: 1.5rem;
+	@media only screen and (min-width: 600px) {
+		grid-template-columns: repeat(2, minmax(0, 1fr));
+	}
+	@media only screen and (min-width: 1200px) {
+		grid-template-columns: repeat(3, minmax(0, 1fr));
+	}
+`;
 export default Bikers;
